b7e15470dad54a5e8cc3bf48f1681a1861954130
Robert Massaioli <rmassaioli@atlassian.com>
2019-12-14 09:30:14 +1100

    Wrote test cases for this entire package.

---
+++ src/index.test.ts
@@ -0,0 +1,94 @@
+import 'jest';
+import { isPresent, isDefined, isFilled } from '.';
+
+type TestData = {
+  data: string;
+};
+
+describe('Functions', () => {
+  describe('isPresent', () => {
+    it('should return true on present', () => {
+      expect(isPresent<TestData>({ data: 'hello world' })).toBeTruthy();
+    });
+
+    it('should return false on undefined', () => {
+      expect(isPresent<TestData>(undefined)).toBeFalsy();
+    });
+
+    it('should return false on null', () => {
+      expect(isPresent<TestData>(null)).toBeFalsy();
+    });
+
+    it('should filter out only present values from an array', () => {
+      const results: Array<TestData | undefined | null> = [
+        { data: 'hello' },
+        null,
+        { data: 'world' },
+        undefined,
+        { data: 'wow'}
+      ];
+
+      const presentResults: Array<TestData> = results.filter(isPresent);
+
+      expect(presentResults).toEqual([
+        { data: 'hello' },
+        { data: 'world' },
+        { data: 'wow'}
+      ])
+    });
+  });
+
+  describe('isDefined', () => {
+    it('should return true on defined', () => {
+      expect(isDefined<TestData>({ data: 'hello world' })).toBeTruthy();
+    });
+
+    it('should return false on undefined', () => {
+      expect(isDefined<TestData>(undefined)).toBeFalsy();
+    });
+
+    it('should filter out only present values from an array', () => {
+      const results: Array<TestData | undefined> = [
+        { data: 'hello' },
+        { data: 'world' },
+        undefined,
+        { data: 'wow'}
+      ];
+
+      const presentResults: Array<TestData> = results.filter(isDefined);
+
+      expect(presentResults).toEqual([
+        { data: 'hello' },
+        { data: 'world' },
+        { data: 'wow'}
+      ])
+    });
+  });
+
+  describe('isFilled', () => {
+    it('should return true on defined', () => {
+      expect(isFilled<TestData>({ data: 'hello world' })).toBeTruthy();
+    });
+
+    it('should return false on null', () => {
+      expect(isFilled<TestData>(null)).toBeFalsy();
+    });
+
+    it('should filter out only present values from an array', () => {
+      const results: Array<TestData | null> = [
+        { data: 'hello' },
+        null,
+        { data: 'world' },
+        { data: 'wow'}
+      ];
+
+      const presentResults: Array<TestData> = results.filter(isFilled);
+
+      expect(presentResults).toEqual([
+        { data: 'hello' },
+        { data: 'world' },
+        { data: 'wow'}
+      ])
+    });
+  });
+});
